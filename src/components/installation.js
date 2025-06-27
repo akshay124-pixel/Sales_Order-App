3389bd37245b03f7d63c4ac6a7980336baf82210
Akshay Pratap Singh <akshay124-pixel@example.com>
2025-06-27 12:03:03 +0530

    Dispatch Date Updated

--- src/components/installation.js
+++ src/components/installation.js
@@ -296,6 +296,7 @@ function Installation() {
         "Shipping Address": order.shippingAddress || "N/A",
         "Docket No": order.docketNo || "N/A",
         Transporter: order.transporter || "N/A",
+        "Dispatch Date": order.dispatchDate || "N/A",
         "Installation Details": order.installation || "N/A",
         "Installation Status": order.installationStatus || "Pending",
         "Sales Person": order.salesPerson || "N/A",
